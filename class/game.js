a53db0b4b860a90f17a04427f72aa35cb5b5fffd
Damien Pinna <damien.pinna@gmail.com>
2023-05-18 20:39:05 +0200

    ajout des methodes displayGameBoard et checkCell dans la calss Game

--- class/game.js
+++ class/game.js
@@ -1,7 +1,11 @@
+import Image from "./image.js";
 import TimeUtils from "./timeUtils.js";
+import GameBoard from "./gameBoard.js";
 
 export default class Game {
-  constructor() {
+  constructor(chronometer) {
+    this.gameBoard = new GameBoard();
+    this.chronometer = chronometer;
     this.content = document.querySelector("#content");
     this.modal = document.querySelector("#modal");
     this.record = document.querySelector(".record");
@@ -11,6 +15,79 @@ export default class Game {
     this.canPlay = true;
     this.score = 0;
   }
+
+  displayGameBoard = () => {
+    let text = "";
+
+    this.gameBoard.gameBoard.forEach((row, indexRow) => {
+      text += "<div>";
+      row.forEach((cell, indexColumn) => {
+        if (cell === 0) {
+          text += `<button class='item' id='button-${indexRow}-${indexColumn}'><span class='text-item'>Dévoiler</span></button>`;
+        } else {
+          text += `<img src='${Image.getImage(
+            cell
+          )}' alt='image' class='p_size-card'/>`;
+        }
+      });
+      text += "</div>";
+    });
+
+    this.content.innerHTML = text;
+
+    this.gameBoard.gameBoard.forEach((row, indexRow) => {
+      row.forEach((cell, indexColumn) => {
+        if (cell === 0) {
+          const button = document.getElementById(
+            `button-${indexRow}-${indexColumn}`
+          );
+          button.addEventListener("click", () => {
+            this.checkCell(`${indexRow}-${indexColumn}`);
+          });
+        }
+      });
+    });
+  };
+
+  checkCell = (cell) => {
+    if (this.canPlay) {
+      this.nbClick++;
+      const row = cell.substr(0, 1);
+      const column = cell.substr(2, 1);
+      this.gameBoard.gameBoard[row][column] =
+        this.gameBoard.resultBoard[row][column];
+      this.displayGameBoard();
+
+      if (this.nbClick > 1) {
+        this.canPlay = false;
+        setTimeout(() => {
+          if (
+            this.gameBoard.gameBoard[row][column] !==
+            this.gameBoard.resultBoard[this.odlSelection[0]][
+              this.odlSelection[1]
+            ]
+          ) {
+            this.gameBoard.gameBoard[row][column] = 0;
+            this.gameBoard.gameBoard[this.odlSelection[0]][
+              this.odlSelection[1]
+            ] = 0;
+          }
+          this.displayGameBoard();
+          this.canPlay = true;
+          this.nbClick = 0;
+          this.odlSelection = [row, column];
+          if (!this.gameBoard.gameBoard.flat().includes(0)) {
+            this.chronometer.stopChronometer();
+            this.score = this.chronometer.seconds;
+            this.viewBestScore(this.score);
+            this.viewModal();
+          }
+        }, 1000);
+      } else {
+        this.odlSelection = [row, column];
+      }
+    }
+  };
 
   viewModal = () => {
     this.content.classList.add("hide");
@@ -23,7 +100,7 @@ export default class Game {
 
     if (score < bestScore) {
       if (bestScore !== 3600) {
-        message.textContent = "Bravo ! Vous avez battu votre reccord !";
+        this.message.textContent = "Bravo ! Vous avez battu votre reccord !";
       }
       bestScore = score;
       localStorage.setItem("bestScoreMemoAnimauxDP", bestScore);
--- main.js
+++ main.js
@@ -1,84 +1,13 @@
-import Image from "./class/image.js";
 import Chronometer from "./class/chronometer.js";
-import GameBoard from "./class/gameBoard.js";
 import Game from "./class/game.js";
 
 const chronometer = new Chronometer();
-const gameBoard = new GameBoard();
-const game = new Game();
+const game = new Game(chronometer);
 const restart = document.querySelector(".restart");
 
 chronometer.startChronometer();
 
-const displayGameBoard = () => {
-  let text = "";
-
-  gameBoard.gameBoard.forEach((row, indexRow) => {
-    text += "<div>";
-    row.forEach((cell, indexColumn) => {
-      if (cell === 0) {
-        text += `<button class='item' id='button-${indexRow}-${indexColumn}'><span class='text-item'>Dévoiler</span></button>`;
-      } else {
-        text += `<img src='${Image.getImage(
-          cell
-        )}' alt='image' class='p_size-card'/>`;
-      }
-    });
-    text += "</div>";
-  });
-
-  game.content.innerHTML = text;
-
-  gameBoard.gameBoard.forEach((row, indexRow) => {
-    row.forEach((cell, indexColumn) => {
-      if (cell === 0) {
-        const button = document.getElementById(
-          `button-${indexRow}-${indexColumn}`
-        );
-        button.addEventListener("click", () => {
-          checkCell(`${indexRow}-${indexColumn}`);
-        });
-      }
-    });
-  });
-};
-
-const checkCell = (cell) => {
-  if (game.canPlay) {
-    game.nbClick++;
-    const row = cell.substr(0, 1);
-    const column = cell.substr(2, 1);
-    gameBoard.gameBoard[row][column] = gameBoard.resultBoard[row][column];
-    displayGameBoard();
-
-    if (game.nbClick > 1) {
-      game.canPlay = false;
-      setTimeout(() => {
-        if (
-          gameBoard.gameBoard[row][column] !==
-          gameBoard.resultBoard[game.odlSelection[0]][game.odlSelection[1]]
-        ) {
-          gameBoard.gameBoard[row][column] = 0;
-          gameBoard.gameBoard[game.odlSelection[0]][game.odlSelection[1]] = 0;
-        }
-        displayGameBoard();
-        game.canPlay = true;
-        game.nbClick = 0;
-        game.odlSelection = [row, column];
-        if (!gameBoard.gameBoard.flat().includes(0)) {
-          chronometer.stopChronometer();
-          game.score = chronometer.seconds;
-          game.viewBestScore(game.score);
-          game.viewModal();
-        }
-      }, 1000);
-    } else {
-      game.odlSelection = [row, column];
-    }
-  }
-};
-
-displayGameBoard();
+game.displayGameBoard();
 
 restart.addEventListener("click", () => {
   location.reload();
