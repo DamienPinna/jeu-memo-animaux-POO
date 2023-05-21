4e655ce56ab680895cca23322951ca3270053480
Damien Pinna <damien.pinna@gmail.com>
2023-05-21 11:11:05 +0200

    ajout du plugin terser pour rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import terser from "@rollup/plugin-terser";
+
 export default {
   input: "main.js",
   output: {
-    file: "./dist/bundle.js",
+    file: "./dist/main.js",
     format: "cjs",
   },
+  plugins: [terser()],
 };
